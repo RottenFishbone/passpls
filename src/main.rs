faf24372596bec199ce8e4b3d234739e8b9bba3a
Jayden Dumouchel <jdumouch@ualberta.ca>
2022-04-11 11:16:46 -0600

    Basic password generation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::borrow::BorrowMut;
+
 use clap::Parser;
+use rand::{rngs::OsRng, RngCore, prelude::SliceRandom, Rng};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
@@ -45,7 +48,49 @@ struct Args {
 }
 
 fn main() {
-    let _args = Args::parse();
+    // Load CLI arguments
+    let args = Args::parse();
+
+    // Seed the RNG (OsRng is cryptographically secure)
+    let mut key = [0u8; 16];
+    OsRng.fill_bytes(&mut key);
+    // TODO combine with seeding from random.org
+
+    // Load our dictionary of word candidates
+    let dict: Vec<String>;
+    if let Some(_dict_path) = args.dict_path {
+        dict = Vec::new();
+    }
+    else{
+        let dict_str = include_str!("../assets/dict.txt");
+        dict = dict_str.lines()
+            .skip_while(|line| line.eq_ignore_ascii_case("---"))
+            .map(|line| String::from(line))
+            .collect();
+    }
+    
+
+    // Choose the number of words to generate
+    let num_words: u8;
+    if args.min_words == args.max_words {
+        num_words = args.max_words;
+    }
+    else {
+        num_words = OsRng.borrow_mut().gen_range(args.min_words..(args.max_words+1));
+    }
+ 
+
+    // Generate a single password
+    let mut words: Vec<String> = Vec::new();
+    for _ in 0..num_words {
+        let word = dict.choose(OsRng.borrow_mut())
+            .expect("Failed to select a word from the dictionary");
+
+        words.push(word.to_owned());
+        words.push(" ".to_string());
+    }
+ 
+    println!("{}", words.concat());
 }
 
 
