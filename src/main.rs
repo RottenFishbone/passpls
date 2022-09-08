bf0861f15cf3bc263bead075c5a317b027d6a25f
Jayden Dumouchel <jdumouch@ualberta.ca>
2022-09-08 15:26:24 -0600

    Split style into an optional (default) feature Integrated a cross-platform clipboard manager Implemented multiple password generation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::borrow::BorrowMut;
-use std::io::Write;
 
 use clap::Parser;
 use rand::{rngs::OsRng, RngCore, prelude::SliceRandom, Rng};
-use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
+
+#[cfg(feature = "style")]
+use std::io::Write;
+#[cfg(feature = "style")]
+use termcolor::{ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
@@ -13,13 +16,16 @@ struct Args {
     dict_path: Option<String>,
     
     /// Do not print the output to stdout
+    ///
+    /// Provided by the 'style' feature
     #[clap(short = 'H', long)]
+    #[cfg(feature = "style")]
     hidden: bool,
     
-
     /// Copies the result to the system clipboard. 
     ///
     /// Multiple passwords will be newline delimited.
+    /// Provided by the 'clipboard' feature.
     #[clap(short, long)]
     #[cfg(feature = "clipboard")]
     copy: bool,
@@ -46,11 +52,15 @@ struct Args {
     #[clap(long)]
     dict_info: bool,
 
+    /// Outputs the contents of the dictionary to stdout
     #[clap(long)]
     dump_dict: bool,
 
     /// Disables color output and carats for piping/interop
+    ///
+    /// Provided by the 'style' feature 
     #[clap(short = 'N', long)]
+    #[cfg(feature = "style")]
     no_style: bool,
 }
 
@@ -76,59 +86,84 @@ fn main() {
         for word in dict.iter() {
             println!("{}", word);
         }
-        return;
+        return
     }
     
     // Seed the RNG (OsRng is cryptographically secure)
     let mut key = [0u8; 16];
     OsRng.fill_bytes(&mut key);
     // TODO optionally combine with seeding from random.org
+    
+    let mut generated: Vec<String> = Vec::new();
+    for _ in 0..args.number {
+        // Choose the number of words to generate
+        let num_words: u8;
+        if args.min_words == args.max_words {
+            num_words = args.max_words;
+        }
+        else {
+            num_words = OsRng.borrow_mut().gen_range(args.min_words..(args.max_words+1));
+        }
 
-    // Choose the number of words to generate
-    let num_words: u8;
-    if args.min_words == args.max_words {
-        num_words = args.max_words;
-    }
-    else {
-        num_words = OsRng.borrow_mut().gen_range(args.min_words..(args.max_words+1));
-    }
- 
+        // Generate a single password
+        let mut words: Vec<String> = Vec::new();
+        for _ in 0..num_words {
+            let word = dict.choose(OsRng.borrow_mut())
+                .expect("Failed to select a word from the dictionary");
 
-    // Generate a single password
-    let mut words: Vec<String> = Vec::new();
-    for _ in 0..num_words {
-        let word = dict.choose(OsRng.borrow_mut())
-            .expect("Failed to select a word from the dictionary");
+            words.push(word.to_owned());
+        }
+        
+        // Output result to stdout in desired manner
+        if cfg!(feature = "style") { #[cfg(feature = "style")] {
+            // Skip printing if output is hidden
+            if args.hidden { continue }
 
-        words.push(word.to_owned());
+            let mut stdout = StandardStream::stdout(ColorChoice::Auto);
+            let mut spec = ColorSpec::new();
+            spec.set_bold(true);
+
+            if !args.no_style {
+                // Print words with bolded initial letters
+                for word in &words {
+                    stdout.set_color(&spec.clone()).unwrap();
+                    write!(&mut stdout, "{}", &word[..1]).unwrap();
+                    stdout.reset().unwrap();
+                    write!(&mut stdout, "{}", &word[1..]).unwrap();
+                }
+                write!(&mut stdout, "\n").unwrap();
+                
+                // Print carats
+                let indicators = words.iter()
+                    .map(|w| {
+                        format!("^{}", (1..w.len()).map(|_| " ".to_string()).collect::<String>())
+                    })
+                .collect::<String>();
+                println!("{}", indicators);
+            }
+            else {
+                // Print unstyled output
+                println!("{}", words.concat());
+            }
+        }}
+        else {
+            // Compiled without styling, simlpy print unstyled
+            println!("{}", words.concat());
+        }
+
+        generated.push(words.concat());
     }
     
-    let mut stdout = StandardStream::stdout(ColorChoice::Auto);
-    let mut spec = ColorSpec::new();
-    spec.set_fg(Some(Color::Red))
-        .set_bold(true);
-
-    // Prevent extra info when outputting to non-terminal caller
-    if args.no_style { 
-        println!("{}", words.concat());            
-        return 
+    // Copy to clipboard, if desired
+    #[cfg(feature = "clipboard")]
+    if args.copy {
+        // Determine the clipboard context from the host's display server
+        copypasta_ext::display::DisplayServer::select()
+            .try_context()
+            .expect("Failed to create a clipboard context.")
+            .set_contents(generated.join("\n"))
+            .expect("Failed to set the contents of your clipboard");
     }
-    
-    for word in &words {
-        stdout.set_color(&spec.clone()).unwrap();
-        write!(&mut stdout, "{}", &word[..1]).unwrap();
-        stdout.reset().unwrap();
-        write!(&mut stdout, "{}", &word[1..]).unwrap();
-    }
-    write!(&mut stdout, "\n").unwrap();
-
-    let indicators = words.iter()
-        .map(|w| {
-            format!("^{}", (1..w.len()).map(|_| " ".to_string()).collect::<String>())
-        })
-        .collect::<String>();
-    println!("{}", indicators);
-    println!("Length: {}", words.concat().len());
 }
 
 
